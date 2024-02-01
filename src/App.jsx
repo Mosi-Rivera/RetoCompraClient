7cf0ec8a5c1cbdd495f68d781bd6f49958d5ee47
Mosi-Rivera <mosiriveraserra@gmail.com>
2024-02-01 18:54:02 -0400

    Remove test default route and commented code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from './pages/SignIn';
-import {ORIGIN_URL} from './api/environment';
-
 import './app.css';
-import DisplayProducts from './components/DisplayProducts/DisplayProducts';
-
-const fetchProducts = async (filters) => {
-  try
-  {
-    const query = '?' + new URLSearchParams(filters);
-    console.log(query);
-    const response = await fetch(ORIGIN_URL + '/api/products' + query);
-    if (response.ok)
-      return response.json();
-    return Promise.reject(res);
-  }
-  catch(err)
-  {
-    console.log(err);
-  }
-}
 
 function App() {
 
   return (
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<DisplayProducts fetchMethod={fetchProducts} loading_backdrop default_sort='popular' default_limit={5}/>}/>
           <Route path='/signin' element={<SignIn />}></Route>
+          <Route path='/register' element={<MyForm/>}></Route>
         </Routes>
       </BrowserRouter>
   );
