b7badf7cc3949df0d9c07c16ec0080a62873d5a4
Janice <janiceqr@gmail.com>
2024-02-13 19:24:22 -0400

    Adding homePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import MyForm from './components/MyForm';
+import MyForm from './pages/MyForm';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from './pages/SignIn';
+import Home from './pages/Home';
+import { AboutUs } from './components/AboutUs';
+// import { loader } from './pages/Home';
+
 import './App.css';
+
 
 function App() {
 
   return (
-      <BrowserRouter>
+    <BrowserRouter>
+
         <Routes>
+          {/* <Route
+            loader={loader}
+            path="/" element={<Home />}></Route> */}
           <Route path='/login' element={<SignIn />}></Route>
           <Route path='/register' element={<MyForm />}></Route>
-        </Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/AboutUs' element={<AboutUs/>}></Route>
+          </Routes>
       </BrowserRouter>
   );
 }
