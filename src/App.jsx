2a83e0b49c51f1a69518e5654ed1aef3fce5f6ce
Mofongo <59919716+Mosi-Rivera@users.noreply.github.com>
2024-02-14 10:24:38 -0400

    Set valid import path.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MyForm from './pages/MyForm';
+import MyForm from './components/MyForm';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
