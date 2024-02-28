edc13389d0060aa9d3515b7c1216ac8b95008f44
Daniel <dlopez0916@gmail.com>
2024-02-28 19:48:17 -0400

    fixHomeBanner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,45 @@
-import { FolderCopyOutlined } from "@mui/icons-material";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, Grid } from "@mui/material";
 import Header from "../components/Header";
 import DisplayProducts from "../components/DisplayProducts/DisplayProducts";
 import { getProducts } from "../api/products/productRoutes";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
-// import "../styles/Home.css";
+
 
 const Home = () => {
   return (
-    <Container maxWidth={false} sx={{maxWidth: 1400 , paddingTop: "95px"}}>
+    <Container maxWidth={false} sx={{ maxWidth: 1400, paddingTop: "95px" }}>
       <Header />
       <main>
         <section>
-          <div  className="full" style={{
-          width: '100%',
-          height: "500px",
-          maxHeight: "100vh",
-          overflow: 'hidden',
-          backgroundImage: "url(/public/assets/Background.jpg)",
-          backgroundSize: "cover",
-          }}> 
-          </div> 
-          <Box sx={{display:"flex" , width: "100%", justifyContent:"space-between", marginTop: "1rem"}}>
-            <Link to="/Men" style={{flex:"1", marginRight: "8px"}} > 
-              <Box sx={{width: "100%", paddingTop: "400px", backgroundSize: "cover", backgroundImage:"url(/public/assets/MenRetoCompras.jpg)"}} > 
-              </Box> 
-              <Typography variant="h4" textAlign="center" >Men</Typography> 
-            </Link>
-            <Link to="/Women" style={{flex:"1", marginLeft: "8px"}} > 
-              <Box sx={{width: "100%" , paddingTop: "400px", backgroundSize: "cover", backgroundImage:"url(/public/assets/WomenRetoCompras.jpg)"}} > 
-                
-              </Box> 
-              <Typography variant="h4" textAlign="center" >Women</Typography> 
-            </Link>
-          </Box>
+          <div className="full" style={{
+            width: '100%',
+          }}>
+            <Box sx={{ display: { xs: "block", md: "none" } }}>
+              <img src="/assets/Background.jpg" style={{ width: "100%" }} />
+            </Box>
+            <Box sx={{ display: { xs: "none", md: "block" } }}>
+              <img src="/assets/Background2.jpeg" style={{ width: "100%" }} />
+            </Box>
+          </div>
+          <Grid container columnSpacing={4} marginTop={3}>
+            <Grid item xs={12} md={6}>
+              <Link to="/Men" >
+                <Box>
+                  <img src="/assets/MenRetoCompras.jpg" style={{ width: "100%" }} />
+                </Box>
+                <Typography variant="h4" textAlign="center" >Men</Typography>
+              </Link>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Link to="/Women" >
+                <Box>
+                  <img src="/assets/WomenRetoCompras.jpeg" style={{ width: "100%" }} />
+                </Box>
+                <Typography variant="h4" textAlign="center" >Women</Typography>
+              </Link>
+            </Grid>
+          </Grid>
         </section>
         <section>
           <h2 style={{ textAlign: 'center' }}>Popular Products</h2>
@@ -42,7 +47,7 @@ const Home = () => {
         </section>
       </main>
       {/* <Footer /> */}
-    </Container>
+    </Container >
   );
 }
 
