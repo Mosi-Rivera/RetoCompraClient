3a1138967a77b734c5f0e08928e0229889355efa
Janice <janiceqr@gmail.com>
2024-03-24 20:15:01 -0400

    Get product info page

--- src/pages/ProductInfo.jsx
+++ src/pages/ProductInfo.jsx
@@ -1,22 +1,24 @@
 import React, { useState , useEffect } from "react"
-import { Button, Container } from "@mui/material";
+import { Button, Container, Grid, Box, Typography } from "@mui/material";
 import Header from "../components/Header";
 import DisplayProducts from "../components/DisplayProducts/DisplayProducts";
 import { getProductInfo} from "../api/products/productRoutes";
-import { useParams } from 'react-router-dom';    
+import { useParams, Link, useNavigate } from 'react-router-dom';    
 
 export default function ProductInfo() {
 
-    // const { userInfo, setUserInfo } = useContext(userContext);
     const {productId} = useParams();
     
     const [productInfo, setProductInfo]= useState(null)
+
+    const navigate = useNavigate()
    
     useEffect(() => {
         async function getInfo() {
             try {
                 const returnedProductInfo = await getProductInfo(productId)
                 setProductInfo(returnedProductInfo) 
+                console.log(returnedProductInfo)
     
             } catch(error) {
                 console.log(error)
@@ -24,10 +26,12 @@ export default function ProductInfo() {
         }
             getInfo()
 
-    })
+    }, [])
+    
         if (!productInfo)
             return <h3> Product not available </h3>
-       
+    
+
     return (
         <Container maxWidth={false} sx={{ maxWidth: 1400, paddingTop: "95px", textAlign: 'left'  }}>
             <Header />
@@ -37,18 +41,37 @@ export default function ProductInfo() {
                 <section>
                     <h2 style={{ textAlign: 'left' }}>Product Information</h2>
 
-                    {/* <Grid container columnSpacing={4} marginTop={3}>
+                    <Grid container columnSpacing={4} marginTop={3}>
                         <Grid item xs={12} md={6}> 
-                        --> Product Image (thumbnail)
+
+                        <img src={productInfo?.variant?.assets?.thumbnail} width="100%" />
+                        
+                     {/* --> Product Image (thumbnail) */}
                 
                         </Grid>
                             <Grid item xs={12} md={6}>
-                            --> Product Description divide by Boxes
-                            --> include button to buy --> not authen 
+                            {/* --> Product Description divide by Boxes
+                            --> include button to buy --> not authen  */}
+                            <Typography variant="h6" gutterBottom>
+                            Description
+                            {productInfo?.variant?.product?.brand}
+
+                           <Grid container >
+                            {productInfo?.colors.map(colorInfo => <Grid item> 
+                                <Link onClick={()=> navigate(0)} to= {"/product/" + colorInfo._id} > <img src={colorInfo?.assets?.thumbnail} width="30px"/>
+                                </Link>
+                                </Grid>)}
+                            </Grid>
+
+                        </Typography>
+                            <Button variant="contained" color="primary">
+                             Add to Cart
+                            </Button>
 
                         </Grid>
-                    </Grid> */}
-{/* 
+
+                    </Grid>
+            {/* 
                         <div>
                         {userInfo.isAuthenticated ?
                             <AuthenticatedNav handleLogout={handleLogout} role={userInfo.user.role} firstName={userInfo.user.firstName} lastName={userInfo.user.lastName} /> :
