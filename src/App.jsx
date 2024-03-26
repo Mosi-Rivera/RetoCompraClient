313d6fef10dbcc169b7e0caaf722ddea261467f5
Janice <janiceqr@gmail.com>
2024-03-26 18:45:05 -0400

    Lastest app version 3/26/24

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,9 @@ import Home from './pages/Home';
 import Men from './pages/Men';
 import Women from './pages/Women';
 import SearchProduct from './pages/SearchProduct'
-<<<<<<< HEAD
-import ProductInfo from './pages/ProductInfo'
-
-import { CrudProducts } from './components/CrudProducts';
-=======
 import Layout from './components/Layout';
+import ProductInfo from './pages/ProductInfo'
 import PageNotFound from './pages/PageNotFound';
->>>>>>> develop
 
 
 function App() {
