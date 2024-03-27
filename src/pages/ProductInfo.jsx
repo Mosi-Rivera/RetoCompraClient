519c77e5b2804cbe4b1b90aee0f8237aebda370d
Janice <janiceqr@gmail.com>
2024-03-26 20:15:26 -0400

    adding stock filter to Size selection

--- src/pages/ProductInfo.jsx
+++ src/pages/ProductInfo.jsx
@@ -1,9 +1,10 @@
-import React, { useState , useEffect } from "react"
+import React, { useState , useEffect, useMemo } from "react"
 import { Button, Container, Grid, Box, Typography, MenuItem, Select } from "@mui/material";
 import Header from "../components/Header";
 import DisplayProducts from "../components/DisplayProducts/DisplayProducts";
 import { getProductInfo} from "../api/products/productRoutes";
 import { useParams, Link, useNavigate } from 'react-router-dom';    
+
 
 const productSize = [
     "XS",
@@ -21,6 +22,15 @@ export default function ProductInfo() {
     const [productInfo, setProductInfo]= useState(null)
     const [selectedStock, setSelectedStock]= useState("")
 
+    function stockFilter(sizeString) {
+        return productInfo?.variant?.stock[sizeString].stock > 0;
+    }
+
+    const sizeItems = useMemo(()=> productSize.filter(stockFilter).map(stockItem => (
+        <MenuItem key={stockItem} value={stockItem}>
+            {stockItem}
+        </MenuItem>
+    )), [productInfo] )  
    
     useEffect(() => {
         async function getInfo() {
@@ -44,10 +54,7 @@ export default function ProductInfo() {
         setSelectedStock(event.target.value)
     };
 
-
     return (
-        <Container maxWidth={false} sx={{ maxWidth: 1400, paddingTop: "95px", textAlign: 'left'  }}>
-            <Header />
             <main>
                 {/* { productInfo ?  : <h3> Product not available </h3>} */}
 
@@ -57,7 +64,7 @@ export default function ProductInfo() {
                     <Grid container columnSpacing={4} marginTop={3}>
                         <Grid item xs={12} md={6}> 
 
-                        <img src={productInfo?.variant?.assets?.thumbnail} width="100%" />
+                        <img src={productInfo?.variant?.assets?.thumbnail} width="80%" />
                         
                      {/* --> Product Image (thumbnail) */}
                 
@@ -72,29 +79,20 @@ export default function ProductInfo() {
                             {productInfo?.variant?.product?.brand} </h3>
                             <h3>Size:
                                     <Select value={selectedStock} onChange={handleStockChange}>
-                                        {productSize.map(stockItem => (
-                                            <MenuItem key={stockItem} value={stockItem}>
-                                                {stockItem}
-                                            </MenuItem>
-                                        ))}
+                                        {sizeItems}
                                     </Select>
-
                             </h3>
                             <h3>Quantity:
                             {productInfo?.variant?.product?.stock} </h3>
-                            
-                    
-                    
                             <h3>Price:
                             ${productInfo?.variant?.price?.value} </h3>
-
-                           <h3> Colors: <Grid container >
-                            {productInfo?.colors.map(colorInfo => <Grid item> 
-                                <Link to= {"/product/" + colorInfo._id} > <img src={colorInfo?.assets?.thumbnail} width="30px"/>
-                                </Link>
+                           <h3> Colors:
+                             <Grid container >
+                                {productInfo?.colors.map(colorInfo => <Grid item> 
+                                    <Link to= {"/product/" + colorInfo._id} > <img src={colorInfo?.assets?.thumbnail} width="30px"/>
+                                    </Link>
                                 </Grid>)}
                             </Grid> </h3>
-
                         </Typography>
                             <Button variant="contained" color="primary">
                              Add to Cart
@@ -114,7 +112,4 @@ export default function ProductInfo() {
              */}
                 </section>
             </main>
-            {/* <Footer /> */}
-        </Container >
-    );
-}
+)}
